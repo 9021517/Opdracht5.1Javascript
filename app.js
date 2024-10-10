6df611a565860d32c240e6d98c25ed3e27d788ee
9021517 <9021517@student.zadkine.nl>
2024-10-10 11:51:12 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+window.onload=function(){
+    var HeadingToAdjust = document.getElementById("TestHeader2");
+    HeadingToAdjust.style.color ="Orange";
+    document.getElementById("TestHeader1").innerHTML = "New tekst!";
+};
